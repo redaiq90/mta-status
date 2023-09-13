07fcbd5191c1cb9164b04ea35c9f317af23f3c64
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2023-09-13 13:03:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const prefix = "*"
-const client = new Discord.Client();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client();
 const fs = require("fs");
 client.prefix = prefix;
 client.commands = new Discord.Collection();
@@ -20,9 +21,122 @@ for (const file of commandFiles) {
   client.commands.set(command.name, command); // by : zef
 }
 
+let lastMessageId = null;
+function sendMTAStatus(channel) {
+  Gamedig.query({
+    type: "mtasa",
+    host: "156.245.5.39",
+    port: "22003",
+  })
+    .then((state) => {
+      let embed = new Discord.MessageEmbed()
+        .setColor("GREEN") // Assuming server is online
+        .setTitle(state["name"])
+        .addField("Status", "Online", true)
+        .addField(
+          "Players",
+          state["raw"]["numplayers"] + "/" + state["maxplayers"],
+          true
+        );
+
+      if (state.players && state.players.length > 0) {
+        // If there are players, list them
+        let playerList = state.players.map((player) => player.name).join(", ");
+        embed.addField("Players Online", playerList);
+      } else {
+        embed.addField("Players Online", "No players online.");
+      }
+
+      // Send the status to the specified channel
+      if (lastMessageId) {
+        // If a message ID exists, attempt to edit the last message
+        channel.messages.fetch(lastMessageId)
+          .then((message) => {
+            message.edit({ embed })
+              .then(() => {
+                console.log('Last message edited.');
+              })
+              .catch((error) => {
+                console.error('Error editing last message:', error);
+              });
+          })
+          .catch(() => {
+            // If the message couldn't be found, send a new one
+            channel.send({ embed })
+              .then((message) => {
+                lastMessageId = message.id; // Store the new message's ID
+                console.log('New message sent.');
+              })
+              .catch((error) => {
+                console.error('Error sending new message:', error);
+              });
+          });
+      } else {
+        // If there's no last message ID, send a new message
+        channel.send({ embed })
+          .then((message) => {
+            lastMessageId = message.id; // Store the new message's ID
+            console.log('New message sent.');
+          })
+          .catch((error) => {
+            console.error('Error sending new message:', error);
+          });
+      }
+    })
+    .catch((error) => {
+      let embed = new Discord.MessageEmbed()
+        .setColor("RED") // Server is offline or query failed
+        .setTitle("Iraq Lions")
+        .addField("Status:", "Offline");
+
+      // Send the status to the specified channel
+      if (lastMessageId) {
+        // If a message ID exists, attempt to edit the last message
+        channel.messages.fetch(lastMessageId)
+          .then((message) => {
+            message.edit({ embed })
+              .then(() => {
+                console.log('Last message edited.');
+              })
+              .catch((error) => {
+                console.error('Error editing last message:', error);
+              });
+          })
+          .catch(() => {
+            // If the message couldn't be found, send a new one
+            channel.send({ embed })
+              .then((message) => {
+                lastMessageId = message.id; // Store the new message's ID
+                console.log('New message sent.');
+              })
+              .catch((error) => {
+                console.error('Error sending new message:', error);
+              });
+          });
+      } else {
+        // If there's no last message ID, send a new message
+        channel.send({ embed })
+          .then((message) => {
+            lastMessageId = message.id; // Store the new message's ID
+            console.log('New message sent.');
+          })
+          .catch((error) => {
+            console.error('Error sending new message:', error);
+          });
+      }
+    });
+}
+
 client.on("ready", () =>{
     console.log(`Logged in as ${client.user.tag}!`); // by : zef
 	client.user.setActivity('Iraq Lions!', { type: 'PLAYING' }) // by : zef
+	setInterval(() => {
+    
+    const channel = client.channels.cache.get('1148501158210568202');
+    if (channel) {
+      sendMTAStatus(channel);
+    }
+  }, 15000); // Every 30 seconds
  });
 
 client.on("message", (message) => {
@@ -41,8 +155,6 @@ client.on("message", (message) => {
 
 const token = process.env.TOKENN;
 client.login(token);
-
-
 
 const port = process.env.PORT || 8080;
 
