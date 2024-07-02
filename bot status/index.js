ec2ebf1e8c07c905ebe234cc5d65b12bd8cbb89c
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2024-07-03 01:31:58 +0300

    Update index.js

--- bot status/index.js
+++ bot status/index.js
@@ -22,7 +22,7 @@ for (const file of commandFiles) {
 
 client.on("ready", () =>{
     console.log(`Logged in as ${client.user.tag}!`); // by : zef
-	client.user.setActivity('Developer : ! Pé , mm0.#0007', { type: 'PLAYING' }) // by : zef
+	client.user.setActivity('! اسود العراق يعمل مجددا', { type: 'PLAYING' }) // by : zef
  });
 
 client.on("message", (message) => {
